dabbc1f2f7e887ee685932e340edb6cd393d6dc9
Chase Carroll <cdoncarroll@gmail.com>
2023-08-18 11:17:49 -0400

    CALayer Recursive Description
    Sometimes it's really helpful to log the layer tree hierarchy out to the console. UIKit already provides `recursiveDescription` on `UIView`, and Chisel provides `pviews` and `pvc`. This commit simply adds a similar mechanism to `CALayer`.

---
+++ Extensions/Print Layer Hierarchy/Swift/CALayer+RecursiveDescription.swift
@@ -0,0 +1,43 @@
+extension CALayer {
+    
+    func recursiveDescription(useDebugDescription: Bool = false) {
+        _recursiveDescription(0, useDebugDescription: useDebugDescription)
+    }
+    
+    private func _recursiveDescription(_ currentLevel: Int, useDebugDescription: Bool) {
+        var descriptionString = ""
+        
+        // Append a string in the beginning that indicates the current depth in the layer hierarchy
+        if currentLevel > 0 {
+            for _ in 1...currentLevel {
+                descriptionString += "   |"
+            }
+        }
+        
+        // Append the description of this layer.
+        descriptionString += (" " + (useDebugDescription ? self.debugDescription : self.description))
+        
+        // Ensure there will be a line break after the description for this layer prints
+        descriptionString += "\n"
+        
+        // Actually print the description
+        print(descriptionString)
+        
+        // If there aren't any child layers, then we can just bail.
+        guard
+            let sublayers = sublayers,
+            sublayers.count > 0
+        else {
+            return
+        }
+        
+        // If there are child layers, recurse down each one of them.
+        for layer in sublayers {
+            layer.recursiveDescription(
+                currentLevel + 1,
+                useDebugDescription: useDebugDescription
+            )
+        }
+    }
+
+}
